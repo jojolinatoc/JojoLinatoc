1908e2c73aec0ebe9f56866bbf8613ec8fe030da
Jojo P. Linatoc <jojolinatoc@kinpo.com.ph>
2016-07-09 15:14:42 +0800

    deploy first section

--- public/javascripts/angular/jojoApp.js
+++ public/javascripts/angular/jojoApp.js
@@ -2,17 +2,19 @@ var jojo = angular.module("jojoApp", []);
 
 jojo.controller("headerCtrl", function($scope) {
     $scope.navLinks = [
-                            {
-                                url: "/Portfolio",
-                                linkName: "Portfolio"
-                            },
-                            {
-                                url: "/About",
-                                linkName: "About"
-                            },
-                            {
-                                url: "/Contact",
-                                linkName: "Contact"
-                            }
-                        ];
+        {
+             url: "/Contact",
+                         linkName: "Contact"
+
+        },
+        {
+            url: "/Portfolio",
+                                  linkName: "Portfolio"
+        },
+        {
+
+            url: "#AboutMe",
+            linkName: "About Me"
+        }
+    ];
 });
