7525d91365e666ea53a4ae4517fb53afd36c3382
Jojo P. Linatoc <jojolinatoc@kinpo.com.ph>
2016-07-25 16:35:55 +0800

    finish about me section

--- public/javascripts/angular/jojoApp.js
+++ public/javascripts/angular/jojoApp.js
@@ -1,6 +1,6 @@
-var jojo = angular.module("jojoApp", []);
+var jojo = angular.module("jojoApp", ['ngSanitize']);
 
-jojo.controller("headerCtrl", function($scope) {
+jojo.controller("websiteCtrl", function($scope) {
     $scope.navLinks = [
         {
              url: "#Contact",
@@ -16,4 +16,58 @@ jojo.controller("headerCtrl", function($scope) {
             linkName: "About Me"
         }
     ];
+
+    $scope.aboutMe = [
+            {
+                 icon: "/public/images/aboutme/cp.png",
+                 aboutItem: "0997 415 0425",
+                 link: "none"
+            },
+            {
+                 icon: "/public/images/aboutme/email.png",
+                 aboutItem: "linatocjojop@gmail.com",
+                 link: "none"
+            },
+            {
+                 icon: "/public/images/aboutme/website.png",
+                 aboutItem: "http://jojo-linatoc.herokuapp.com/",
+                 link: "http://jojo-linatoc.herokuapp.com/"
+            },
+            {
+                 icon: "/public/images/aboutme/address.png",
+                 aboutItem: "#186, Lumbang, Lipa City, Batangas",
+                 link: "none"
+            },
+        ];
+
+        $scope.softwares = [
+            {
+                 iconSoft: "/public/images/aboutme/play.png",
+                 aboutSoft: "This is it!",
+            },
+            {
+                 iconSoft: "/public/images/aboutme/sourceTree.png",
+                 aboutSoft: "This is it!",
+            },
+            {
+                 iconSoft: "/public/images/aboutme/intellij.png",
+                 aboutSoft: "This is it!",
+            },
+            {
+                 iconSoft: "/public/images/aboutme/adobe.png",
+                 aboutSoft: "This is it!",
+            },
+        ];
+
+        $scope.hobbies = [
+            {
+                 iconH: "/public/images/aboutme/music.png",
+            },
+            {
+                 iconH: "/public/images/aboutme/cinema.png",
+            },
+            {
+                 iconH: "/public/images/aboutme/windows.png",
+            },
+        ];
 });
